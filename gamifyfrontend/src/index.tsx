c1d554445d7aff1e5e00f6031394dcfb6b6d7507
Daniel <daniel.sofran@stud.ubbcluj.ro>
2023-04-12 04:02:45 +0300

    favicon added

--- gamifyfrontend/src/index.tsx
+++ gamifyfrontend/src/index.tsx
@@ -19,20 +19,8 @@ root.render(
             </Routes>
         </AuthProvider>
       </BrowserRouter>
-      {/*<script src="https://cdn.jsdelivr.net/npm/react/umd/react.production.min.js" crossOrigin="anonymous"></script>*/}
-
-      {/*<script*/}
-      {/*    src="https://cdn.jsdelivr.net/npm/react-dom/umd/react-dom.production.min.js" crossOrigin="anonymous"></script>*/}
-
-      {/*<script*/}
-      {/*    src="https://cdn.jsdelivr.net/npm/react-bootstrap@next/dist/react-bootstrap.min.js" crossOrigin="anonymous"></script>*/}
-
-      <script>var Alert = ReactBootstrap.Alert;</script>
   </React.StrictMode>
 
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
